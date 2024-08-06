c85003a20afea191ec5f4cc8610feb1ed2186602
jagadeesh-vana <jagadeesh.vana@gmail.com>
2024-08-06 10:50:40 +0530

    calculate train & eval losses, generate from model

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ n = int(0.9 * len(data))
 train_data = data[:n]
 val_data = data[n:]
 
-def split(split):
+def get_batch(split):
     data = train_data if split == 'train' else val_data
     idx = torch.randint(0, len(data) - block_size, (batch_size,))
     x = torch.stack([data[i: i+ block_size] for i in idx])
@@ -32,14 +32,37 @@ block_size = 8
 batch_size = 32
 n_layer = 2
 
-m = GPTLanguageModel(vocab_size, n_embd, block_size, n_layer)
-optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+model = GPTLanguageModel(vocab_size, n_embd, block_size, n_layer)
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
 
-for _ in range(10):
-    x, y = split('train')
-    logits, loss = m(x, y)
+max_iters = 10
+eval_interval = 4
+eval_iters = 2
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+for iter in range(max_iters):
+    if iter//eval_interval == 0 or iter == max_iters-1:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+    x, y = get_batch('train')
+    logits, loss = model(x, y)
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
-print(loss)
+# generate from the model
+context = torch.zeros((1, 1), dtype=torch.long)
+print(decode(model.generate(context, block_size, max_new_tokens=500)[0].tolist()))
